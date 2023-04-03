648fc1cc9ebd52544b9f8e6c8a30dad8807f1b91
Abeinevincent <abeinevincent@gmail.com>
2023-04-03 10:18:26 +0300

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/layout/header/Navbar";
 import Booking from "./pages/booking/Booking";
 import Bookings from "./pages/booking/Bookings";
-import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/register/Register";
 const router = createBrowserRouter([
